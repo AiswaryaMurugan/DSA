56caee4b99a21581913f8ffcfbd2737eaf9f4a9f
Aiswarya Murugan <aiswaryamurugan200@gmail.com>
2026-07-15 13:46:35 +0530

    Add files via upload

---
+++ Day11/BTFromInPost.java
@@ -0,0 +1,42 @@
+package Day11;
+
+import java.util.Stack;
+
+import Day10.LevelOrder;
+import Day10.TreeNode;
+
+public class BTFromInPost {
+    public static TreeNode buildTree(int[] in, int[] post){
+        if(in==null||post==null||in.length==0) return null;
+        int iIndex=in.length-1, pIndex=post.length-1;
+        Stack<TreeNode> stk = new Stack<>();
+        TreeNode root = new TreeNode(post[pIndex]);pIndex--;
+        stk.push(root);
+        while(pIndex>=0){
+            TreeNode node = stk.peek();
+            if(node.getVal()!=in[iIndex]){
+                TreeNode right = new TreeNode(post[pIndex]);
+                pIndex--;
+                node.setRight(right);
+                stk.push(right);
+            }
+            else{
+                while(!stk.isEmpty()&&
+                        stk.peek().getVal()==in[iIndex]){
+                    node = stk.pop();iIndex--;
+                }
+                if(pIndex>=0){
+                    TreeNode left = new TreeNode(post[pIndex]);
+                    pIndex--;node.setLeft(left);stk.push(left);
+                }
+            }
+        }
+        return root;
+    }
+    public static void main(String[] args) {
+        int[] pOrder = {9,15,7,20,3};
+        int[] iOrder = {9,3,15,20,7};
+        TreeNode root = buildTree(iOrder, pOrder);
+        System.out.println(LevelOrder.orderLevel(root));
+    }
+}
